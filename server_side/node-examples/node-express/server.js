2d6637e9680769600b7078e731d4f67755ac88aa
Alexandr Bobrenko <oknerbob@gmail.com>
2016-06-06 09:31:23 +0300

    node js - rest

--- server_side/node-examples/node-express/server.js
+++ server_side/node-examples/node-express/server.js
@@ -5,7 +5,7 @@ var express = require('express');
 var morgan = require('morgan');
 
 // позволяет читать body запроса к серверу
-var bodyParser = require('body-parse');
+var bodyParser = require('body-parser');
 
 var hostname = 'localhost';
 var port = 3000;
@@ -20,15 +20,34 @@ app.use(bodyParser.json());
 // вызывает функцию, когда приходит запрос на /dishes
 app.all('/dishes', function (req, res, next){
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  next();
+  next(); // позволяет продолжить выполнение
+});
+
+// по гету отдает что-то. Например, достает из БД
+// next не вызывается, так что выполнения функции процесс прерывается
+app.get('/dishes', function (req, res, next){
+  res.end('any data');
+});
+
+// по посту добавляет новую информацию
+app.post('/dishes', function (req, res, next){
+  res.end('Will add data ' + req.body.name + ' with details: ' + req.body.description);
+});
+
+// по достает итем по ID. Добавить и delete - тоже самое
+app.get('/dishes/:dishId', function (req, res, next){
+  res.end('Will add data ' + req.params.dishId);
+});
+
+// модифицировать уже имеюийся итем
+app.put('/dishes/:dishId', function (req, res, next){
+  res.write('Updating data: ' + req.params.dishId + '/n');
+  res.end('Will update data ' + req.body.name + 'with details: ' + req.body.description);
 });
 
 // любой запрос (например about.html) будет искаться в папке public
-// __dirname - абсолютный путь к папке (оттуда, где лежит express)
-// так что сервер можно запускать откуда угодно
 app.use(express.static(__dirname + '/public'));
 
-// короткая форма инициализации http модуля через express
 app.listen(port, hostname, function (){
   console.log('Server running at http ' + hostname + ':' + port);
 });
