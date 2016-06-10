07d8d2c8901276d1cecbab0e53f7039d0ba1bc7a
Alexandr Bobrenko <oknerbob@gmail.com>
2016-06-10 09:27:05 +0300

    node - mongoose

--- server_side/node-examples/node-mongoose/server.js
+++ server_side/node-examples/node-mongoose/server.js
@@ -6,4 +6,17 @@ var dishSchema = new Schema({
   description: {type: String, required: true}
 }, {timestamps: true});
 
-var Dishes = mongoose.Modle('Dish', dishSchema);
+
+var commentSchema = new Schema(
+  { rating: {type: Number, min: 1, max: 5, required: true},
+    comment: {type: String, required: true},
+    author: {type: String, required: true}},
+  {timestamps: true});
+
+var dishSchema = new Schema(
+  { name: {type: String, required: true, unique: true},
+    description: {type: String, required: true},
+    comments: [commentSchema]},
+  {timestamps: true});
+
+var Dishes = mongoose.Model('Dish', dishSchema);
